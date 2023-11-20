e0d202deed680e19809bc579970a6130d64cf64b
ARUNKUMARKONGI <arunkongi@gmail.com>
2023-11-20 12:02:06 +0530

    shopping cart final updated work

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,9 @@ class Item {
         this.name = name;
         this.price = price;
     }
-
     public String getName() {
         return name;
     }
-
     public double getPrice() {
         return price;
     }
@@ -25,54 +23,61 @@ public class Main {
         TreeMap<String, List<Item>> carts = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
         carts.put("Grocery", new ArrayList<>());
         carts.put("Electronics", new ArrayList<>());
-        carts.put("Clothes", new ArrayList<>());
+        carts.put("Clothing", new ArrayList<>());
 
         System.out.println("Welcome to the Shopping App!");
         System.out.println("Available Carts: Grocery, Electronics, Clothes");
         System.out.println("Choose a Cart to Add Products:");
-
+        System.out.println("\n1. Grocery Cart");
+        System.out.println("2. Electronics Cart");
+        System.out.println("3. Clothing Cart");
+        System.out.println("4. Place Order");
+        System.out.println("5. Exit\n");
         while (true) {
-            System.out.println("\n1. Grocery Cart");
-            System.out.println("2. Electronics Cart");
-            System.out.println("3. Clothes Cart");
-            System.out.println("4. Place Order");
-
             int choice = sc.nextInt();
             sc.nextLine();
-
             switch (choice) {
                 case 1:
-                    addProductToCart(carts.get("Grocery"));
+                    addItemToCart(carts.get("grocery"),"GROCERY");
                     break;
                 case 2:
-                    addProductToCart(carts.get("Electronics"));
+                    addItemToCart(carts.get("Electronics"),"ELECTRONICS");
                     break;
                 case 3:
-                    addProductToCart(carts.get("Clothes"));
+                    addItemToCart(carts.get("Clothing"),"CLOTHING");
                     break;
                 case 4:
                     placeOrder(carts);
                     break;
+                case 5:sc.close();
+                    System.out.println("Exiting Shopping App");
+                    System.exit(0);
                 default:
                     System.out.println("Invalid choice. Please choose a valid option.");
             }
         }
     }
 
-    private static void addProductToCart(List<Item> cart) {
-        System.out.println("Enter the name of the product:");
+    private static void addItemToCart(List<Item> cart, String cartName) {
+        // System.out.println("Enter the name of the product:");
         String itemName = sc.next();
         double itemPrice = sc.nextDouble();
         sc.nextLine();
         Item item = new Item(itemName, itemPrice);
         cart.add(item);
-        System.out.println(itemName + " has been added to the cart");
+        System.out.println(itemName + " has been added to the cart "+cartName);
     }
 
     private static double calculateTotal(List<Item> cart) {
         return cart.stream().mapToDouble(Item::getPrice).sum();
     }
-
+    /*private static double calculateTotal(List<Item> cart) {
+        double total = 0.0;
+        for (Item item : cart) {
+            total += item.getPrice();
+        }
+        return total;
+    }*/
     private static void removeItemFromCart(List<Item> cart, String itemName) {
         cart.removeIf(item -> item.getName().equalsIgnoreCase(itemName));
         System.out.println(itemName + " has been removed from the cart");
@@ -80,10 +85,10 @@ public class Main {
 
     private static void placeOrder(TreeMap<String, List<Item>> carts) {
         while (true) {
-            System.out.println("\nEnter Cart Name to Place Order:");
-            System.out.println("Grocery");
+            //System.out.println("\nEnter Cart Name to Place Order:");
+            /*System.out.println("Grocery");
             System.out.println("Electronics");
-            System.out.println("Clothes");
+            System.out.println("Clothing");*/
             String cartName = sc.nextLine();
             List<Item> selectedCart = carts.get(cartName);
             if (selectedCart == null || selectedCart.isEmpty()) {
@@ -92,10 +97,10 @@ public class Main {
                 break;
             }
             double total = calculateTotal(selectedCart);
-            System.out.println("Do you want to remove any product from the " + cartName + " Cart? (yes/no)");
+            // System.out.println("Do you want to remove any product from the " + cartName + " Cart? (remove/no)");
             String remove = sc.nextLine().toLowerCase();
-            if (remove.equals("yes")) {
-                System.out.println("Enter the name of the product to remove:");
+            if (remove.equals("remove")) {
+                // System.out.println("Enter the name of the product to remove:");
                 String itemToRemove = sc.nextLine();
                 removeItemFromCart(selectedCart, itemToRemove);
                 total = calculateTotal(selectedCart);
@@ -105,24 +110,31 @@ public class Main {
                 discount = 0.1;
             } else if (cartName.equalsIgnoreCase("Electronics")) {
                 discount = 0.2;
-            } else if (cartName.equalsIgnoreCase("Clothes")) {
+            } else if (cartName.equalsIgnoreCase("Clothing")) {
                 discount = 0.3;
             }
             double discountedTotal = total * (1 - discount);
+
             System.out.println("\nOrder Summary:");
             System.out.println("Items in the " + cartName + " Cart:");
-            for (Item item : selectedCart) {
+            for (Item item : selectedCart)
                 System.out.println("- " + item.getName() + " : Rs." + item.getPrice() + "/-");
-            }
 
-            System.out.printf("\n%s Cart - Total Bill (10%% discount applied): Rs.%.2f/-\n", cartName, discountedTotal);
+           // selectedCart.stream().forEach(item -> System.out.println("- "+ item.getName()+" "+ item.getPrice()));
+
+            System.out.printf("\n%s CART - Total Bill (%d%% discount applied): Rs.%.2f/-\n", cartName.toUpperCase(), (int)(discount * 100), discountedTotal);
+
             System.out.println("\nOrder Placed!!Thank You!!!");
-            System.out.println("Do you want to Order from Other Carts?");
-            String continueShopping = sc.nextLine().toLowerCase();
-            if (continueShopping.equals("no")) {
+            //System.out.println("Do you want to Order from Other Carts? (continue/no)");
+
+            selectedCart.clear(); //clear the cart after the order is placed.
+
+            String flag = sc.nextLine().toLowerCase();
+            if (flag.equals("no")) {
                 System.out.println("Returning to Main Site. Continue Shopping");
                 break;
             }
         }
     }
 }
+
